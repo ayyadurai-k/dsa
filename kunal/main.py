e0a62119fcb083575f682ef1fbbdaaff5581385b
Ayyadurai K <kmayyadurai286@gmail.com>
2026-02-01 09:19:10 +0530

    feat: Implement pattern_24 function for star pattern printing

--- kunal/main.py
+++ kunal/main.py
@@ -13,7 +13,23 @@ def pattern_17(n):
             print(col,end="")
         print("")
         
-
+def pattern_24(n):
+    for row in range(1,n*2+1):
+        effective_row = row if row<=n else n*2 - row+1
+        stars = effective_row
+        spaces = (2*n) - (effective_row*2)
+        
+        left_star_end = stars
+        right_star_start = stars+spaces+1
+        
+        for col in range(1,n*2+1):
+            if col == left_star_end or right_star_start == col or col==1 or col==2*n:
+                print("*",end="")
+            else:
+                print(" ",end="")
+        print("")
+            
 
 if __name__ == "__main__":
-    pattern_17(5)
+    # pattern_17(5)
+    pattern_24(5)
