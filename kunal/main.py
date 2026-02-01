df851995c92c0f34b46654e663dddb567d64f90d
Ayyadurai K <kmayyadurai286@gmail.com>
2026-02-01 08:41:54 +0530

    feat: Implement pattern_17 function for diamond pattern printing

--- kunal/main.py
+++ kunal/main.py
@@ -1,2 +1,17 @@
+
+
+def pattern_17(n):
+    for row in range(1,n*2):
+        effective_row = row if row<=n else n*2 - row 
+        spaces = n-effective_row        
+        
+        for col in range(spaces):
+            print(" ",end="")
+        for col in range(effective_row,0,-1):
+            print(col,end="")
+        for col in range(2,effective_row+1):
+            print(col,end="")
+        print("")
+
 if __name__ == "__main__":
-    print("Hi")
+    pattern_17(5)
